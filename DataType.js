5fb70c4d681c4905e7cde5672be0b30c7de6196a
unknown <rajeshbtech009@gmail.com>
2025-11-25 17:47:04 +0530

    First Commit

---
+++ DataType.js
@@ -0,0 +1,22 @@
+let firstName = "Rajesh"
+console.log(firstName)
+console.log(typeof firstName)
+
+let companyName = "Epam"
+console.log(companyName)
+console.log(typeof companyName)
+
+
+
+let mobileNumber = 9629568779
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+
+let isAutomation = true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
